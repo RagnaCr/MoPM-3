910fe7dc003d135ad5a0ca5a586f4c6a7b8cd5f2
RagnaCr <lpayrick@gmail.com>
2023-12-13 23:34:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoPM_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
